d3d602aab75a17696213ed7db4f0b835152f7de2
Philipp Jardas <philipp@jardas.de>
2018-01-31 15:29:12 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-export default () => <div>Hello, World!</div>;
+import WelcomePage from './pages/WelcomePage';
+import AboutPage from './pages/AboutPage';
+
+export default () => (
+  <Router>
+    <div>
+      <h1>Serious Retrospective</h1>
+      <ul>
+        <li>
+          <NavLink to="/" exact>
+            Welcome
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  </Router>
+);
